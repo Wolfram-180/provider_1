ff0114b5c50bb42db523fca98b67e640bf96dc45
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-08 22:41:28 +0300

    class BreadCrumbProvider extends ChangeNotifier {

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
@@ -23,5 +26,47 @@ class HomePage extends StatelessWidget {
         title: const Text('Home page'),
       ),
     );
+  }
+}
+
+class BreadCrumb {
+  bool isActive;
+  final String name;
+  final String uuid;
+
+  BreadCrumb({
+    required this.isActive,
+    required this.name,
+  }) : uuid = const Uuid().v4();
+
+  void activate() {
+    isActive = true;
+  }
+
+  @override
+  bool operator ==(covariant BreadCrumb other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  String get title => isActive ? '$name > ' : name;
+}
+
+class BreadCrumbProvider extends ChangeNotifier {
+  final List<BreadCrumb> _items = [];
+
+  UnmodifiableListView<BreadCrumb> get item => UnmodifiableListView(_items);
+
+  void add(BreadCrumb breadCrumb) {
+    for (final item in _items) {
+      item.activate();
+    }
+    _items.add(breadCrumb);
+    notifyListeners();
+  }
+
+  void reset() {
+    _items.clear();
+    notifyListeners();
   }
 }
